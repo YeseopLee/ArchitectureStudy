a60c094614fb642c0f972693397a4f7e4d2cf10e
yesaeop lee <yeseoplee94@gmail.com>
2021-01-04 23:34:45 +0900

    Repository

--- app/src/main/java/com/example/architecture/ui/MainActivity.kt
+++ app/src/main/java/com/example/architecture/ui/MainActivity.kt
@@ -13,45 +13,17 @@ import com.example.architecture.R
 import com.example.architecture.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private lateinit var binding: ActivityMainBinding
     private lateinit var vm : MainViewModel
-    private lateinit var myApplication: MyApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
 
-        inject()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
+        //viewModel 연결
         vm = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.viewModel = vm
 
-    }
-
-    private fun inject(){
-        myApplication = application as MyApplication
-    }
-
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val focusView = currentFocus
-
-        if (focusView != null) {
-            val rect = Rect()
-            focusView.getGlobalVisibleRect(rect)
-            val x = ev!!.x.toInt()
-            val y = ev.y.toInt()
-            if (!rect.contains(x, y)) {
-                val imm: InputMethodManager =
-                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                if (imm != null) imm.hideSoftInputFromWindow(focusView.windowToken, 0)
-                focusView.clearFocus()
-            }
-        }
-        return super.dispatchTouchEvent(ev)
     }
 
 }
